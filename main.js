8d148d0b9d60e9a65fa96bf1a76c0875e12969e3
Ebrahim Abd EL Razik <94077806+ebrahim-0@users.noreply.github.com>
2023-01-29 23:22:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const secHand = document.querySelector(".sec-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const sec = now.getSeconds();
+  const secDegree = (sec / 60) * 360 + 180;
+
+  const mins = now.getMinutes();
+  const minDegree = (mins / 60) * 360 + 180;
+
+  const hours = now.getHours();
+  const hourDegree = (hours / 12) * 360 + 180;
+
+  console.log(hourDegree);
+
+  secHand.style.transform = `rotate(${secDegree}deg)`;
+  minHand.style.transform = `rotate(${minDegree}deg)`;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
